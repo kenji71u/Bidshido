d64be74ca5063e8032ce839373dae59d26fab0af
Steven Putra <xiau_xu_pen@yahoo.com>
2021-05-16 15:27:33 +0800

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/**/*.html', 
+    './src/**/*.ts',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
